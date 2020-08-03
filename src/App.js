164633e350575085183c83e209c4ae316f2a7023
Alfonso Dalix <50084031+Alfondalix@users.noreply.github.com>
2020-08-03 19:39:53 -0300

    complete covid-tracker project

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import { fetchData } from './api';
+
+import styles from './App.module.css';
+
+import coronaImage from './img/image.png';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    cuntry: '',
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
+
+    //set the state
+  };
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="logo" />
+
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
